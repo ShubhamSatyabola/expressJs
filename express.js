6d624ae0405bbc9fda031fd2e90bee83aa478cca
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-03-19 22:22:28 +0530

    routing in express and use bodyparser

--- express.js
+++ express.js
@@ -1,14 +1,15 @@
 
 const express = require('express');
-
+const parser = require('body-parser')
 const app = express();
-app.use((req,res,next)=>{
-    next()
+app.use(parser.urlencoded())
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button>send</button><form/>')
 })
-app.use((req,res,next)=>{
-    res.send({'key1':'value1'})
-
+app.use('/product',(req,res,next)=>{
+    console.log(req.body.title)
+    console.log(req.body.size)
+    res.redirect('/add-product')
 })
-
 
 app.listen(3000)
