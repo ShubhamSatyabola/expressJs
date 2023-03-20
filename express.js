dedcb1065d9b69b978464d657abc7723d8c1c02f
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-03-20 08:07:03 +0530

    using router and filter

--- express.js
+++ express.js
@@ -1,15 +1,19 @@
-
 const express = require('express');
-const parser = require('body-parser')
-const app = express();
-app.use(parser.urlencoded())
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button>send</button><form/>')
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body.title)
-    console.log(req.body.size)
-    res.redirect('/add-product')
-})
 
-app.listen(3000)
+const parser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(parser.urlencoded());
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>page not found</h1>')
+});
+
+app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/',(req,res,next)=>{
+    
+    res.send('hello from express-js!')
+})
+
+module.exports = router
